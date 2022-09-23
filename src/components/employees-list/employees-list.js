468ef98493fa535da43b1875d8e4a45758bd25c6
alex-borisova <alex.pineapplle@gmail.com>
2022-09-23 16:37:24 +0300

    upd epl list and added onClick functions

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,15 +1,18 @@
 import React from "react";
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 import "./employees-list.css";
+import PropTypes from "prop-types";
 
-const EmployeesList = () => {
-  return (
-    <ul className="app-list list-group">
-      <EmployeesListItem />
-      <EmployeesListItem />
-      <EmployeesListItem />
-    </ul>
-  );
+const EmployeesList = ({ data }) => {
+  const elements = data.map((item) => {
+    const { id, ...itemProps } = item;
+    return <EmployeesListItem {...itemProps} key={id} />;
+  });
+
+  return <ul className="app-list list-group">{elements}</ul>;
+};
+EmployeesList.propTypes = {
+  data: PropTypes.array.isRequired,
 };
 
 export default EmployeesList;
